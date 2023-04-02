fa7489d9721bf48b0d3b1aabbf138e55d5a26151
chahatmittal <mittalchahat754@gmail.com>
2023-04-02 20:56:07 +0530

    useState added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,47 +1,45 @@
-import restaurantData from '../utils/restaurantData';
-import RestaurantCard from './RestaurantCard';
+import { useState } from "react";
+
+import resData from "../utils/restaurantData";
+import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-    return (
-        <main className="main__content">
-            <div className="filter__container">
-                <div className="filter__row">
-                    <button
-                        className="filter__btn"
-                        onClick={() => {
-                            console.log("Filter");
-                        }}
-                    >
-                        Relevance
-                    </button>
+  const [restaurantsData, setRestaurantsData] = useState(resData);
 
-                    <button
-                        className="filter__btn"
-                        onClick={() => {
-                            console.log("Filter");
-                        }}
-                    >
-                        Top Rated Restaurant
-                    </button>
+  return (
+    <main className="main__content">
+      <div className="filter__container">
+        <div className="filter__row">
+          <button
+            className="filter__btn"
+            onClick={() => {
+              setRestaurantsData(resData);
+            }}
+          >
+            Relevance
+          </button>
 
-                    <button
-                        className="filter__btn"
-                        onClick={() => {
-                            console.log("Filter");
-                        }}
-                    >
-                        Fast Delivery Time
-                    </button>
-                </div>
-                <input type="text" className="search" placeholder="Search" />
-            </div>
-            <div className="res__container res__card__container">
-                {
-                    restaurantData.map((restaurant) => <RestaurantCard key={restaurant.data.id} restData={restaurant} />)
-                }
-            </div>
-        </main>
-    );
+          <button
+            className="filter__btn"
+            onClick={() => {
+              const filteredData = restaurantsData.filter(
+                (res) => res.data.avgRating > 4
+              );
+              setRestaurantsData(filteredData);
+            }}
+          >
+            Top Rated Restaurant
+          </button>
+        </div>
+        <input type="text" className="search" placeholder="Search" />
+      </div>
+      <div className="res__container res__card__container">
+        {restaurantsData.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} restData={restaurant} />
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default Body;
